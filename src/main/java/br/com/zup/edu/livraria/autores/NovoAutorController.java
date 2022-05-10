a0f7a6331bddcc615844fcc2213b50182619b7fc
Rafael Ponte <rafael.ponte@zup.com.br>
2022-05-10 16:13:58 -0300

    melhorias;

--- src/main/java/br/com/zup/edu/livraria/autores/NovoAutorController.java
+++ src/main/java/br/com/zup/edu/livraria/autores/NovoAutorController.java
@@ -1,15 +1,19 @@
 package br.com.zup.edu.livraria.autores;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import javax.validation.Valid;
 import java.net.URI;
+
+import static org.springframework.http.HttpStatus.UNPROCESSABLE_ENTITY;
 
 @RestController
 public class NovoAutorController {
@@ -20,6 +24,10 @@ public class NovoAutorController {
     @Transactional
     @PostMapping("/api/autores")
     public ResponseEntity<?> cadastra(@RequestBody @Valid NovoAutorRequest request, UriComponentsBuilder uriBuilder) {
+
+        if (repository.existsByEmail(request.getEmail())) {
+            throw new ResponseStatusException(UNPROCESSABLE_ENTITY, "autor com este email já existente");
+        }
 
         Autor autor = request.toModel();
         repository.save(autor);
--- src/main/java/br/com/zup/edu/livraria/livros/NovoLivroController.java
+++ src/main/java/br/com/zup/edu/livraria/livros/NovoLivroController.java
@@ -2,15 +2,19 @@ package br.com.zup.edu.livraria.livros;
 
 import br.com.zup.edu.livraria.autores.AutorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import javax.validation.Valid;
 import java.net.URI;
+
+import static org.springframework.http.HttpStatus.UNPROCESSABLE_ENTITY;
 
 @RestController
 public class NovoLivroController {
@@ -24,6 +28,10 @@ public class NovoLivroController {
     @Transactional
     @PostMapping("/api/livros")
     public ResponseEntity<?> cadastra(@RequestBody @Valid NovoLivroRequest request, UriComponentsBuilder uriBuilder) {
+
+        if (repository.existsByIsbn(request.getIsbn())) {
+            throw new ResponseStatusException(UNPROCESSABLE_ENTITY, "livro com este ISBN já existente");
+        }
 
         Livro livro = request.toModel(autorRepository);
         repository.save(livro);
